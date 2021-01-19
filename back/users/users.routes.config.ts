c5ee878636eb1654c7be1c926774c3ddc887d65e
kboysm <lishal12@naver.com>
2021-01-19 12:43:37 +0900

    backend userRoute

--- back/users/users.routes.config.ts
+++ back/users/users.routes.config.ts
@@ -7,6 +7,34 @@ export class UsersRoutes extends CommonRoutesConfig {
         super( app , 'UsersRoutes' );
     }
     configureRoutes() {
-        return this.app;
+        
+        this.app.route('/users')
+            .get( (req: express.Request, res: express.Response) => {
+                res.status(200).send( 'List Of Users') //DB 생성 후 유저 목록 반환 예정
+            })
+        
+        this.app.route('/users')
+            .post( (req: express.Request, res: express.Response) => {
+                res.status(200).send( 'List Of Users') //DB 생성 후 유저 추가 로직
+            })
+        
+        this.app.route('./users/:userId')
+            .all( (req: express.Request, res: express.Response, next: express.NextFunction) => {
+                next();
+            })
+            .get( (req: express.Request, res: express.Response) => {
+                res.status(200).send(`GET requested for id ${req.params.userId}`);
+            })
+            .put( (req: express.Request, res: express.Response) => {
+                res.status(200).send(`PUT requested for id ${req.params.userId}`);
+            })
+            .patch( (req: express.Request, res: express.Response) => {
+                res.status(200).send(`PATCH requested for id ${req.params.userId}`);
+            })
+            .delete( (req: express.Request, res: express.Response) => {
+                res.status(200).send(`DELETE requested for id ${req.params.userId}`);
+            })
+
+            return this.app;
     }
 }
