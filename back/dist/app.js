80d51d75d0943d4b2ce4993d876b8dd95fa5a186
kboysm <lishal12@naver.com>
2021-01-19 20:04:34 +0900

    backend 배포 테스트 전 커밋

--- back/dist/app.js
+++ back/dist/app.js
@@ -1,13 +1,61 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const app = express_1.default();
-app.get('/', (req, res, next) => {
-    res.send('hello ');
+var express_1 = __importDefault(require("express"));
+var http = __importStar(require("http"));
+var bodyparser = __importStar(require("body-parser"));
+var winston = __importStar(require("winston"));
+var expressWinston = __importStar(require("express-winston"));
+var cors_1 = __importDefault(require("cors"));
+var users_routes_config_1 = require("./users/users.routes.config");
+var debug_1 = __importDefault(require("debug"));
+var app = express_1.default();
+var server = http.createServer(app);
+var port = 3000;
+var routes = [];
+var debugLog = debug_1.default('app');
+app.use(bodyparser.json());
+app.use(cors_1.default());
+app.use(expressWinston.logger({
+    transports: [
+        new winston.transports.Console()
+    ],
+    format: winston.format.combine(winston.format.colorize(), winston.format.json())
+}));
+routes.push(new users_routes_config_1.UsersRoutes(app));
+app.use(expressWinston.errorLogger({
+    transports: [
+        new winston.transports.Console()
+    ],
+    format: winston.format.combine(winston.format.colorize(), winston.format.json())
+}));
+app.get('/', function (req, res) {
+    res.status(200).send('Server Up');
 });
-app.listen(5000, () => {
-    console.log('Server Running ');
+server.listen(port, function () {
+    debugLog("Server running at http://localhost:" + port);
+    routes.forEach(function (route) {
+        debugLog("Routes configured from " + route.getName());
+    });
 });
---
+++ back/dist/users/users.routes.config.js
@@ -0,0 +1,52 @@
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.UsersRoutes = void 0;
+var common_routes_config_1 = require("../common/common.routes.config");
+var UsersRoutes = /** @class */ (function (_super) {
+    __extends(UsersRoutes, _super);
+    function UsersRoutes(app) {
+        return _super.call(this, app, 'UsersRoutes') || this;
+    }
+    UsersRoutes.prototype.configureRoutes = function () {
+        this.app.route('/users')
+            .get(function (req, res) {
+            res.status(200).send('List Of Users'); //DB 생성 후 유저 목록 반환 예정
+        });
+        this.app.route('/users')
+            .post(function (req, res) {
+            res.status(200).send('List Of Users'); //DB 생성 후 유저 추가 로직
+        });
+        this.app.route('./users/:userId')
+            .all(function (req, res, next) {
+            next();
+        })
+            .get(function (req, res) {
+            res.status(200).send("GET requested for id " + req.params.userId);
+        })
+            .put(function (req, res) {
+            res.status(200).send("PUT requested for id " + req.params.userId);
+        })
+            .patch(function (req, res) {
+            res.status(200).send("PATCH requested for id " + req.params.userId);
+        })
+            .delete(function (req, res) {
+            res.status(200).send("DELETE requested for id " + req.params.userId);
+        });
+        return this.app;
+    };
+    return UsersRoutes;
+}(common_routes_config_1.CommonRoutesConfig));
+exports.UsersRoutes = UsersRoutes;
