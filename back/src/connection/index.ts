60bd7974498365d20f9e187501c9750a7796acb8
kboysm <lishal12@naver.com>
2021-02-02 21:29:09 +0900

    서버 분리

--- back/src/connection/index.ts
+++
@@ -1,5 +0,0 @@
-import { createConnection } from "typeorm";
-import { Users } from "../entity/User";
-import  testUserList  from '../testData/index'
-
-export const myConnection = createConnection();
--- back/src/secretKey/index.ts
+++
@@ -1,6 +0,0 @@
-const keyList = {
-    cryptoKey : 'lsm',
-    jwtKey : 'limsm'
-}
-// 실제 상용 프로젝트면 gitignore에 추가
-export default keyList;
