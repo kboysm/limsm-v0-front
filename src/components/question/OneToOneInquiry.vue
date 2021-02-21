934fee3a7c4e67928ed20bc951921504f4aa90ef
kboysm <lishal12@naver.com>
2021-02-21 18:30:09 +0900

    1:1문의 ui

--- src/components/question/OneToOneInquiry.vue
+++ src/components/question/OneToOneInquiry.vue
@@ -1,10 +1,16 @@
 <template>
     <v-card class="px-12 mt-2" flat> 
         <v-card-title class="text-h5">1:1문의</v-card-title>
-        <!-- <vue-editor v-model="content"></vue-editor> -->
+        <v-sheet
+        color="white"
+        elevation="10"
+        height="600"
+        >
+            <vue-editor class="limsm-height" v-model="content"></vue-editor>
+        </v-sheet>
         <br>
         <v-card-actions>
-            <v-btn>제출</v-btn>
+            <v-btn elevation="5" text outlined @click="testFunc">제출</v-btn>
         </v-card-actions>
     </v-card>
 </template>
@@ -20,9 +26,15 @@
     })
     export default class OneToOneInquiry extends Vue {
         content= "<h1>제목: </h1>"
+        
+        testFunc() {
+            console.log(this.content)
+        }
     }
 </script>
 
 <style scoped>
-
+.limsm-height {
+    height: 92%;
+}
 </style>
