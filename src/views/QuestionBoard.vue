3c709cd903993c289731d7463364ff4318c65d73
kboysm <lishal12@naver.com>
2021-02-24 09:54:46 +0900

    question board list

--- src/views/QuestionBoard.vue
+++ src/views/QuestionBoard.vue
@@ -3,7 +3,7 @@
         <v-card width="100%">
             <v-card-text class="text-center display-2 white--text text-accent-3 red">Question Board</v-card-text>
             <hr>
-            <v-data-table height="auto" :headers="columnas" :items="articulos" class="elevation-19">
+            <v-data-table height="auto" :headers="columnas" :items="questionList" class="elevation-19">
             </v-data-table>
         </v-card>
     </v-container>
@@ -12,62 +12,63 @@
 <script lang="ts">
     import { Component, Vue } from 'vue-property-decorator';
 
-    @Component
+    interface question {
+                        id:string,
+                        productName:string,
+                        writer:string,
+                        date:string
+    }
+    interface user {
+        address: string,
+        age: number,
+        createdAt: string,
+        email: string,
+        name: string,
+        id: number,
+        tel: string
+    }
+    interface questionList {
+        id:string,
+        contentName:string,
+        createdAt:string
+        updatedAt:string
+        user: user
+    }
+    @Component<QuestionBoard>({
+        created() {
+            this.getList()
+        }
+    })
     export default class QuestionBoard extends Vue {
         columnas= [
             {text:'ID', value:'id', class:'red derken-4 white--text'},
-            {text:'DESCRIPTION', value:'description', class:'red derken-4 white--text'},
-            {text:'PRECOo', value:'precio', class:'red derken-4 white--text'},
-            {text:'STOCK', value:'stock', class:'red derken-4 white--text'}
+            {text:'상품명', value:'productName', class:'red derken-4 white--text'},
+            {text:'글쓴이', value:'writer', class:'red derken-4 white--text'},
+            {text:'날짜', value:'date', class:'red derken-4 white--text'}
         ]
 
-        articulos= [
-            {
-                id:1,
-                description:'text1',
-                precio:180.00,
-                stock:45
-            },
-            {
-                id:2,
-                description:'text2',
-                precio:90.00,
-                stock:88
-            },
-            {
-                id:3,
-                description:'text3',
-                precio:100.00,
-                stock:27
-            },
-            {
-                id:4,
-                description:'text4',
-                precio:70.00,
-                stock:35
-            },
-            {
-                id:5,
-                description:'text5',
-                precio:70.00,
-                stock:35
-            },
-            {
-                id:6,
-                description:'text6',
-                precio:70.00,
-                stock:35
-            },
-            {
-                id:7,
-                description:'text7',
-                precio:70.00,
-                stock:35
-            }
+        questionList: Array<question>= [
+            
         ]
 
-        openDiglog() {
-            console.log('open dialog')
+        getList() {
+            this.$axios.get('/questions')
+            .then( r=> {
+                console.log(r.data);
+                r.data.forEach( (item:questionList) => {
+                    const listItem:question = {
+                        id:'',
+                        productName:'',
+                        writer:'',
+                        date:''
+                    }
+                    listItem.id = item.id
+                    listItem.productName = item.contentName.split("_lsm")[0]
+                    listItem.writer = item.user.name
+                    listItem.date = item.createdAt
+                    this.questionList.push(listItem)
+                })
+            })
         }
     }
 </script>
